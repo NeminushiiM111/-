5bc1a4435d70015c5ebf245679a2b678a84d16cf
vodka1234Z <timofeew.bogdan2017@yandex.ru>
2025-05-21 16:16:40 +0300

    Update труба.cpp

--- труба/труба.cpp
+++ труба/труба.cpp
@@ -3,5 +3,16 @@ using namespace std;
 int main()
 {
     setlocale (0,"");
-    double A, p, t;
+    double t, Hg, d, L, Q;
+    cout << "Введите t" << endl;
+    cin >> t;
+    cout << "Введите Hg" << endl;
+    cin >> Hg;
+    cout << "Введите d" << endl;
+    cin >> d;
+    cout << "Введите L" << endl;
+    cin >> L;
+    cout << "Введите Q" << endl;
+    cin >> Q;
+    
 }
